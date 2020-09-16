5e47b0b521f8e4d95f3f0c0fe0b38889bb778685
Galih Arum <galiharum181100@gmail.com>
2020-09-16 14:13:32 +0700

    first commit

---
+++ biodata.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (){
+	
+	printf("====================BIODATA====================\n");
+	printf("Nama \t\t\t = Galih Arum Prabowo \n");
+	printf("NPM \t\t\t = 19081010181 \n");
+	printf("Asal \t\t\t = Surabaya \n");
+	printf("Tempat/Tgl Lahir \t = Surabaya, 18 November 2000 \n ");
+	printf("Fakultas \t\t = Fakultas Ilmu Komputer \n");
+	printf("Program Studi \t\t = Informatika ");
+}		
